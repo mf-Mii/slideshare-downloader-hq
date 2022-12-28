6812fca0adf255ca85aef148bd4daa09967d65f5
mf-Mii <67968408+mf-Mii@users.noreply.github.com>
2022-12-29 01:51:20 +0900

    Download hq image and more
    Changed python version from both 2 and 3 to only 3.
    Made the program to download the highest quality image.
    Add option to delete temporary files.
    Changed using library from urllib3 to requests.
    Changed output PDF filename to slide title.

--- convertpdf.py
+++ convertpdf.py
@@ -1,77 +1,82 @@
 #!/usr/bin/env python
-#-*- coding: utf-8 -*-
-import sys, os
-import img2pdf
+# -*- coding: utf-8 -*-
+import os
 import re
+import sys
 
-from time import localtime, strftime
-from os import listdir, walk
-from os.path import isfile, join
-
-try: from urllib.request import urlopen #python3
-except ImportError: from urllib2 import urlopen #python2
-try: from bs4 import BeautifulSoup #python3
-except ImportError: from BeautifulSoup import BeautifulSoup #python2
-try: input = raw_input #python2
-except NameError: pass #python3
+import img2pdf
+import requests as requests
+from bs4 import BeautifulSoup
 
 CURRENT = os.path.dirname(__file__)
+REQUEST_HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/94.0.4606.61 Safari/537.36 '
+}
+
 
 def download_images(url):
-    html = urlopen(url).read()
-    soup = BeautifulSoup(html)
-    title = '_'.join(( 'pdf_images', strftime("%Y/%m/%d_%H:%M:%S", localtime()) ))  #soup.title.string
-    images = soup.findAll('img', {'class':'slide_image'})
+    html = requests.get(url).content
+    soup = BeautifulSoup(html, 'html.parser')
 
-    for image in images:
-        image_url = image.get('data-full').split('?')[0]
-        command = "wget '%s' -P '%s' --no-check-certificate" % (image_url, title)
-        os.system(command)
+    slide_name = os.path.basename(url)
+    slide_title = soup.find('title').text
+    slides = soup.findAll('div', {'class': 'slide'})
+    slide_files = []
 
-    convert_pdf(title)
+    # Create images directory
+    os.mkdir(slide_name)
 
-def convert_pdf(img_dir_name):
-    f = []
-    for (dirpath, dirnames, filenames) in walk(join(CURRENT, img_dir_name)):
-        f.extend(filenames)
-        break
-    f = ["%s/%s" % (img_dir_name, x) for x in f]
-    
-    def atoi(text):
-        return int(text) if text.isdigit() else text
+    for slide in slides:
+        sources = slide.find('source').get('srcset').split(', ')
+        hq_src = None
+        hq_size = 0
+        slide_no = int(slide.get('data-index'))
+        for src in sources:
+            if int(src.split(' ')[1].replace('w', '')) > hq_size:
+                hq_size = int(src.split(' ')[1].replace('w', ''))
+                hq_src = src.split(' ')[0].rsplit('?', 1)[0]
+        print(f'[Slide #{slide_no}] Size: {hq_size}, Src: {hq_src}')
+        raw_img = requests.get(hq_src, headers=REQUEST_HEADERS).content
+        with open(slide_name + '/' + os.path.basename(hq_src), 'wb') as f:
+            f.write(raw_img)
+        slide_files.insert(slide_no, slide_name + '/' + os.path.basename(hq_src))
+    global pdf_f
+    pdf_f = slide_title + '.pdf'
+    convert_pdf(slide_files)
 
-    def natural_keys(text):
-        '''
-        alist.sort(key=natural_keys) sorts in human order
-        http://nedbatchelder.com/blog/200712/human_sorting.html
-        (See Toothy's implementation in the comments)
-        '''
-        return [ atoi(c) for c in re.split('(\d+)', text) ]
 
-    f.sort(key=natural_keys)
-    
-    print(f)
-
-    pdf_bytes = img2pdf.convert(f, dpi=300, x=None, y=None)
+def convert_pdf(img_files_array):
+    pdf_bytes = img2pdf.convert(img_files_array)
     doc = open(pdf_f, 'wb')
     doc.write(pdf_bytes)
     doc.close()
 
+
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         url = " ".join(sys.argv[1:])
+        if len(sys.argv) > 2 and sys.argv[2].lower() == '--del':
+            del_tmp = True
+        else:
+            del_tmp = False
     else:
         url = input('Slideshare URL: ').strip()
+        del_tmp = input('Do you want to delete temp folder?[Y/n]: ').lower().split()[0] == 'y'
     if (url.startswith("'") and url.endswith("'")) or (url.startswith('"') and url.endswith('"')):
         url = url[1:-1]
     if not url.startswith(('http://', 'https://')):
         url = 'https://' + url
-    pdf_f = re.sub('[^0-9a-zA-Z]+', '_', url.split("/")[-1]) #get url basename and replace non-alpha with '_'
+    pdf_f = re.sub('[^0-9a-zA-Z]+', '_', url.split("/")[-1])  # get url basename and replace non-alpha with '_'
     if pdf_f.strip() == '':
         print("Something wrong to get filename from URL, fallback to result.pdf")
         pdf_f = "result.pdf"
     else:
-        pdf_f+=".pdf"
+        pdf_f += ".pdf"
     download_images(url)
-
-
+    print('Download Complete!!')
+    if del_tmp and os.path.exists(os.path.basename(url)):
+        for file in os.listdir(path=os.path.basename(url)):
+            os.remove(os.path.basename(url)+'/'+file)
+        os.removedirs(os.path.basename(url))
+        print('All temp file deleted!!')
--- readme.md
+++ readme.md
@@ -1,23 +1,28 @@
-# Slideshare Downloader
+# Slideshare Downloader HQ
 
-This python script to download slide from Slideshare and converted into pdf automatically.
+This python script to download high quality slide from Slideshare and converted into pdf automatically.
+
+**Only Python version 3 is supported. Python 2 cannot be used.**
+
 
 ## How to setup
     git clone https://github.com/limkokhole/slideshare-downloader.git
 
-    pip2 install -r requirements_py2.txt #python 2
-
-    OR
-
-    pip3 install -r requirements_py3.txt #python 3
+    pip3 install -r requirements.txt
 
 ## How to run
 
-    python convertpdf.py [url]
+    python convertpdf.py [url] [--del]
 
 
 Then it will asked the slideshare url if you doesn't provided [url], example url which doesn't provide download button:
 
 https://www.slideshare.net/reconvillage/rv-defcon25-how-to-obtain-100-facebook-accounts-per-day-through-internet-searches-guillermo-buendia
 
+The [--del] option deletes the temporary folder. Do not add this option when operating in interactive mode (when no URL is specified as an argument).
+
+---
+
 Original fork from https://github.com/yodiaditya/slideshare-downloader.git
+
+Modded by [mf_Mii](https://github.com/mf-Mii/)
